488befb264a04212f5b6e8391f18ad6138ba2769
zh99998 <zh99998@zh99998-pc>
2011-12-19 21:17:45 +0800

    加入适用于linux的sh脚本启动

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,4 @@
 #encoding: UTF-8
-=begin
-alias gbk_puts puts
-def puts(*args)
-  gbk_puts(*(args.collect{|item|item.encode "GBK", :invalid => :replace, :undef => :replace}))
-end
-
-def p(*args)
-  print(args.collect{|item|item.inspect.encode "GBK", :invalid => :replace, :undef => :replace}.join("\n")+"\n") rescue print(args.join("\n")+"\n")
-end
-=end
 def filesize_inspect(size)
   case size
   when 0...1024
@@ -20,15 +10,14 @@ def filesize_inspect(size)
   end
 end
 
-
 require 'sdl'
 include SDL
 require 'yaml'
 $config = YAML.load_file("config.yml") rescue YAML.load_file("data/config_default.yml")
 
 SDL.init(INIT_VIDEO | INIT_AUDIO)
-WM::set_caption("iDuel - 享受决斗", "graphics/system/iDuelPanel_32512.ico")
-WM::icon = Surface.load("graphics/system/iDuelPanel_32512.ico")
+WM::set_caption("MyCard", "graphics/system/icon.gif")
+WM::icon = Surface.load("graphics/system/icon.gif")
 
 style = HWSURFACE
 style |= FULLSCREEN if $config["fullscreen"]
--- mycard.cmd
+++ mycard.cmd
@@ -1,4 +1,4 @@
 cd /d %~dp0
-echo ------------------ >> log.txt
-echo ------------------ >> err.txt
-ruby\bin\ruby lib/main.rb 0>>log.txt 2>>err.txt
+echo ------------------ >> log.log
+echo ------------------ >> err.log
+ruby\bin\ruby lib/main.rb 0>>log.log 2>>err.log
