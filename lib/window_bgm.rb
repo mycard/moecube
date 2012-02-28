9d6c04cd7d7acc2196b6f1e853646e2c84452ecb
zh99998 <zh99998@gmail.com>
2012-02-28 17:10:32 +0800

    BGM标题动画纵向

--- lib/window_bgm.rb
+++ lib/window_bgm.rb
@@ -1,22 +1,23 @@
 require_relative 'window'
 class Window_BGM < Window
+  WLH=20
   def initialize(bgm_name)
     @bgm_name = bgm_name
     @font = TTF.open("fonts/WenQuanYi Micro Hei.ttf", 18)
     width = @font.text_size("♪#{@bgm_name}")[0]
     @count = 0
     @contents = @font.render_blended_utf8("♪#{@bgm_name}" , 255,255,255)
-    super($config['screen']['width'], 0, width, 24,999)
+    super($config['screen']['width']-width, -WLH, width, WLH,999)
   end
   def update
     if @count >= 180
-      if @x >= $config['screen']['width']
+      if @y <= -WLH
         self.destroy
       else
-        self.x += 1.5
+        self.y -= 1
       end
-    elsif $config['screen']['width'] - @x < @width
-      self.x -= 1.5
+    elsif @y < 0
+      self.y += 1
     else
       @count += 1
     end
