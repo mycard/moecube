59e18392335744fd0d2eac2dcd55f986f460da75
zh99998 <zh99998@gmail.com>
2012-07-18 07:07:16 +0800

    temp

--- lib/main.rb
+++ lib/main.rb
@@ -14,19 +14,55 @@ begin
   def save_config(file="config.yml")
     File.open(file, "w") { |file| YAML.dump($config, file) }
   end
+
   def register_url_protocol
     if RUBY_PLATFORM["win"] || RUBY_PLATFORM["ming"]
       require 'win32/registry'
-      pwd = Dir.pwd.gsub('/', '\\')
-      path = '"' + pwd + '\ruby\bin\rubyw.exe" -C"' + pwd + '" -KU lib/main.rb'
-      command = path + ' "%1"'
-      icon = '"' + pwd + '\mycard.exe", 0'
-      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard') { |reg| reg['URL Protocol'] = path.ljust path.bytesize unless (reg['URL Protocol'] == path rescue false) }
-      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard\shell\open\command') { |reg| reg[nil] = command.ljust command.bytesize unless (reg[nil] == command rescue false) }
-      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard\DefaultIcon') { |reg| reg[nil] = icon.ljust icon.bytesize unless (reg[nil] == icon rescue false) }
-      Win32::Registry::HKEY_CLASSES_ROOT.create('.ydk') { |reg| reg[nil] = 'mycard' unless (reg[nil] == 'mycard' rescue false) }
-      Win32::Registry::HKEY_CLASSES_ROOT.create('.yrp') { |reg| reg[nil] = 'mycard' unless (reg[nil] == 'mycard' rescue false) }
-      Win32::Registry::HKEY_CLASSES_ROOT.create('.deck') { |reg| reg[nil] = 'mycard' unless (reg[nil] == 'mycard' rescue false) }
+      path, command, icon = assoc_paths
+      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard') { |reg| reg['URL Protocol'] = path.ljust path.bytesize }
+      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard\shell\open\command') { |reg| reg[nil] = command.ljust command.bytesize }
+      Win32::Registry::HKEY_CLASSES_ROOT.create('mycard\DefaultIcon') { |reg| reg[nil] = icon.ljust icon.bytesize }
+      Win32::Registry::HKEY_CLASSES_ROOT.create('.ydk') { |reg| reg[nil] = 'mycard' }
+      Win32::Registry::HKEY_CLASSES_ROOT.create('.yrp') { |reg| reg[nil] = 'mycard' }
+      Win32::Registry::HKEY_CLASSES_ROOT.create('.deck') { |reg| reg[nil] = 'mycard' }
+    end
+  end
+
+  def assoc_paths
+    pwd = Dir.pwd.gsub('/', '\\')
+    path = '"' + pwd + '\ruby\bin\rubyw.exe" -C"' + pwd + '" -KU lib/main.rb'
+    command = path + ' "%1"'
+    icon = '"' + pwd + '\mycard.exe", 0'
+    [path, command, icon]
+  end
+
+  def assoc_need?
+    return false unless RUBY_PLATFORM["win"] || RUBY_PLATFORM["ming"]
+    return false if $config['no_assoc']
+    path, command, icon = assoc_paths
+    require 'win32/registry'
+    begin
+      Win32::Registry::HKEY_CLASSES_ROOT.open('mycard') { |reg| return true unless reg['URL Protocol'] == path }
+      Win32::Registry::HKEY_CLASSES_ROOT.open('mycard\shell\open\command') { |reg| return true unless reg[nil] == command }
+      Win32::Registry::HKEY_CLASSES_ROOT.open('mycard\DefaultIcon') { |reg| return true unless reg[nil] == icon }
+      Win32::Registry::HKEY_CLASSES_ROOT.open('.ydk') { |reg| return true unless reg[nil] == 'mycard' }
+      Win32::Registry::HKEY_CLASSES_ROOT.open('.yrp') { |reg| return true unless reg[nil] == 'mycard' }
+      Win32::Registry::HKEY_CLASSES_ROOT.open('.deck') { |reg| return true unless reg[nil] == 'mycard' }
+    rescue
+      return true
+    end
+  end
+
+  def request_assoc
+    require_relative 'widget_msgbox'
+    Widget_Msgbox.new("mycard", "即将进行文件关联, 弹出安全警告请点允许", ok: "确定", cancel: "取消") do |clicked|
+      if clicked == :ok
+        yield
+      else
+        Widget_Msgbox.new("mycard", "没有进行关联,要重新关联请删除config.yml")
+        $config['no_assoc'] = true
+        save_config
+      end
     end
   end
 
@@ -53,6 +89,7 @@ begin
         require_relative 'quickstart'
         $scene = false
       when /register_web_protocol/
+        $assoc_requested = true
         register_url_protocol
         $scene = false
     end
@@ -99,14 +136,19 @@ begin
 
     #初始化标题场景
     require_relative 'scene_title'
+    require_relative 'dialog'
     $scene = Scene_Title.new
 
     #自动更新
     require_relative 'update'
     Update.start
     WM::set_caption("MyCard v#{Update::Version}", "MyCard")
-    require_relative 'dialog'
-    register_url_protocol rescue Dialog.uac("ruby/bin/rubyw.exe", "-KU lib/main.rb register_web_protocol")
+    if assoc_need?
+      request_assoc do
+        register_url_protocol rescue Dialog.uac("ruby/bin/rubyw.exe", "-KU lib/main.rb register_web_protocol")
+      end
+    end
+
     $log.info("main") { "初始化成功" }
   end
 rescue Exception => exception
