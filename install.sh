3f1d14e84cce866d8890162627c4c6f2ad6354d3
zh99998 <zh99998@gmail.com>
2013-03-04 20:42:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
-Version=1.0
-Name=mycard
-Name[zh_CN]=萌卡
-Comment=a card game
+Name=Mycard
+Name[zh_CN]=Mycard - 萌卡
+Comment=a card game platform
 Comment[zh_CN]=卡片游戏对战客户端
-Exec=ruby lib/main.rb
+Exec=/usr/bin/env ruby -KU lib/main.rb %u
 Terminal=false
-Icon=$(dirname "$0")/graphics/system/icon.gif
+Icon=$(dirname "$0")/graphics/system/icon.png
 Type=Application
 Categories=Game
-Path=$(dirname "$0")" > ~/.local/share/applications/mycard.desktop
+Path=$(dirname "$0")
+URL=http://my-card.in/
+MimeType=x-scheme-handler/mycard;application/x-ygopro-deck;application/x-ygopro-replay" > ~/.local/share/applications/mycard.desktop
+
+echo "<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/x-ygopro-deck">
+		<comment>ygopro deck</comment>
+		<icon name="application-x-ygopro-deck"/>
+		<glob-deleteall/>
+		<glob pattern="*.ydk"/>
+	</mime-type>
+</mime-info>" > ~/.local/share/mime/packages/application-x-ygopro-deck.xml 
+
+echo "<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+        <mime-type type="application/x-ygopro-replay">
+                <comment>ygopro replay</comment>
+                <icon name="application-x-ygopro-replay"/>
+                <glob-deleteall/>
+                <glob pattern="*.yrp"/>
+        </mime-type>
+</mime-info>" > ~/.local/share/mime/packages/application-x-ygopro-replay.xml 
+
+xdg-mime default mycard.desktop application/x-ygopro-deck
+xdg-mime default mycard.desktop application/x-ygopro-replay
+xdg-mime default mycard.desktop x-scheme-handler/mycard
+
+update-mime-database ~/.local/share/mime
+update-desktop-database
