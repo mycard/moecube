fe7d561672c1d442fff350aa42c28767123182f3
zh99998 <zh99998@gmail.com>
2013-03-04 20:52:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+base=$(cd "$(dirname "$0")"; pwd)
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Name=Mycard
@@ -7,10 +8,10 @@ Comment=a card game platform
 Comment[zh_CN]=卡片游戏对战客户端
 Exec=/usr/bin/env ruby -KU lib/main.rb %u
 Terminal=false
-Icon=$(dirname "$0")/graphics/system/icon.png
+Icon=$base/graphics/system/icon.png
 Type=Application
 Categories=Game
-Path=$(dirname "$0")
+Path=$base
 URL=http://my-card.in/
 MimeType=x-scheme-handler/mycard;application/x-ygopro-deck;application/x-ygopro-replay" > ~/.local/share/applications/mycard.desktop
 
@@ -36,8 +37,8 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 
 xdg-mime default mycard.desktop application/x-ygopro-deck application/x-ygopro-replay x-scheme-handler/mycard
 
-xdg-icon-resource install --context mimetypes --size 256 $(dirname "$0")/graphics/system/icon.png application/x-ygopro-deck
-xdg-icon-resource install --context mimetypes --size 256 $(dirname "$0")/graphics/system/icon.png application/x-ygopro-replay
+xdg-icon-resource install --context mimetypes --size 256 $base/graphics/system/icon.png application/x-ygopro-deck
+xdg-icon-resource install --context mimetypes --size 256 $base/graphics/system/icon.png application/x-ygopro-replay
 
 update-mime-database ~/.local/share/mime
 update-desktop-database
