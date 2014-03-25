1e69ab0026dd063a9dbc10db161cdea784979d95
zh99998 <zh99998@gmail.com>
2014-03-25 18:28:59 +0800

    fix中文验证

--- app/candy/candy.fix.js
+++ app/candy/candy.fix.js
@@ -359,3 +359,64 @@ Candy.View = function(self, $) {
     };
     return self;
 }(Candy.View || {}, jQuery);
+
+//中文验证，看起来SHA1、MD5、PLAIN都对中文支持有问题。通过更换base64库可以修复PLAIN，然后在这里只允许PLAIN认证
+Strophe.Connection.prototype._connect_cb = function(req, _callback, raw) {
+    Strophe.info("_connect_cb was called");
+    this.connected = true;
+    var bodyWrap = this._proto._reqToData(req);
+    if (!bodyWrap) {
+        return;
+    }
+    if (this.xmlInput !== Strophe.Connection.prototype.xmlInput) {
+        if (bodyWrap.nodeName === this._proto.strip && bodyWrap.childNodes.length) {
+            this.xmlInput(bodyWrap.childNodes[0]);
+        } else {
+            this.xmlInput(bodyWrap);
+        }
+    }
+    if (this.rawInput !== Strophe.Connection.prototype.rawInput) {
+        if (raw) {
+            this.rawInput(raw);
+        } else {
+            this.rawInput(Strophe.serialize(bodyWrap));
+        }
+    }
+    var conncheck = this._proto._connect_cb(bodyWrap);
+    if (conncheck === Strophe.Status.CONNFAIL) {
+        return;
+    }
+    this._authentication.sasl_scram_sha1 = false;
+    this._authentication.sasl_plain = false;
+    this._authentication.sasl_digest_md5 = false;
+    this._authentication.sasl_anonymous = false;
+    this._authentication.legacy_auth = false;
+    // Check for the stream:features tag
+    var hasFeatures = bodyWrap.getElementsByTagName("stream:features").length > 0;
+    if (!hasFeatures) {
+        hasFeatures = bodyWrap.getElementsByTagName("features").length > 0;
+    }
+    var mechanisms = bodyWrap.getElementsByTagName("mechanism");
+    var matched = [];
+    var i, mech, auth_str, hashed_auth_str, found_authentication = false;
+    if (!hasFeatures) {
+        this._proto._no_auth_received(_callback);
+        return;
+    }
+    if (mechanisms.length > 0) {
+        for (i = 0; i < mechanisms.length; i++) {
+            mech = Strophe.getText(mechanisms[i]);
+            if(mech == 'SCRAM-SHA-1' || mech == 'DIGEST-MD5') continue
+            if (this.mechanisms[mech]) matched.push(this.mechanisms[mech]);
+        }
+    }
+
+    this._authentication.legacy_auth = bodyWrap.getElementsByTagName("auth").length > 0;
+    found_authentication = this._authentication.legacy_auth || matched.length > 0;
+    if (!found_authentication) {
+        this._proto._no_auth_received(_callback);
+        return;
+    }
+    console.log(matched)
+    if (this.do_authentication !== false) this.authenticate(matched);
+}
