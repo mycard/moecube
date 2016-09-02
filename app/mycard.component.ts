863bb5580bf043ef38c628c0bc42fa6a8723e89b
h3remi7 <a@ako.moe>
2016-09-02 17:32:06 +0800

    add mycard css

--- app/mycard.component.ts
+++ app/mycard.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+declare var process;
 @Component({
   selector: 'mycard',
   templateUrl: 'app/mycard.component.html',
   styleUrls: ['app/mycard.component.css'],
+
 })
-export class MyCardComponent { }
+
+export class MyCardComponent {
+  platform = process.platform;
+}
