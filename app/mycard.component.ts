4e19ed9bc56205ff60af2e629afe1c0934636996
zh99998 <zh99998@gmail.com>
2016-09-02 16:26:24 +0800

    routing

--- app/mycard.component.ts
+++ app/mycard.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RoutingService } from './routing.service';
 @Component({
   selector: 'mycard',
   templateUrl: 'app/mycard.component.html',
   styleUrls: ['app/mycard.component.css'],
 })
-export class MyCardComponent { }
+export class MyCardComponent {
+  constructor(private routingService: RoutingService) { }
+}
