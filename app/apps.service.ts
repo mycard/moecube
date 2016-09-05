6df98e853e6878dd41f37077d9f3a06001171347
zh99998 <zh99998@gmail.com>
2016-09-05 16:20:49 +0800

    apps service

--- app/apps.service.ts
+++ app/apps.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Http,Response } from '@angular/http';
-//import { Observable } from 'rxjs';
-//import 'rxjs/add/operator/map'
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/Rx';
 
 @Injectable()
 export class AppsService {
-    constructor(private http: Http) {}
-    data = '';
+    constructor(private http: Http) {
+    }
 
+    data = '';
 
 
     getApps() {
         console.log(123);
-        this.http.get('./apps.json').map((res) => {console.log(res)})//.subscribe(res => this.data = res);
+        this.http.get('./apps.json')
+            .map(response => response.json())
+            .subscribe(data => this.data = data);
     }
 
 }
