ba33561f9240ecae413632266bfc4debd903de87
zh99998 <zh99998@gmail.com>
2016-09-08 11:11:31 +0800

    外观

--- app/candy.component.ts
+++ app/candy.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import {RoutingService} from "./routing.service";
 @Component({
     selector: 'candy',
     templateUrl: 'app/candy.component.html',
     styleUrls: ['app/candy.component.css'],
 })
-export class CandyComponent { }
+export class CandyComponent {
+    constructor(private routingService: RoutingService){}
+}
--- app/mycard.module.ts
+++ app/mycard.module.ts
@@ -1,26 +1,27 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule }    from '@angular/http';
+import {NgModule, NO_ERRORS_SCHEMA}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule}    from '@angular/http';
 
-import { MyCardComponent }  from './mycard.component';
-import { LoginComponent } from './login.component';
-import { StoreComponent } from './store.component';
-import { LobbyComponent } from './lobby.component';
-import { AppsComponent } from './apps.component';
-import { AppDetailComponent } from './app-detail.component';
-import { RosterComponent } from './roster.component';
-import { CandyComponent } from './candy.component';
-import { CommunityComponent } from './community.component';
+import {MyCardComponent}  from './mycard.component';
+import {LoginComponent} from './login.component';
+import {StoreComponent} from './store.component';
+import {LobbyComponent} from './lobby.component';
+import {AppsComponent} from './apps.component';
+import {AppDetailComponent} from './app-detail.component';
+import {RosterComponent} from './roster.component';
+import {CandyComponent} from './candy.component';
+import {CommunityComponent} from './community.component';
 
-import { RoutingService } from './routing.service';
-import { AppsService } from './apps.service';
-import { AppService } from './app.service';
-
+import {RoutingService} from './routing.service';
+import {AppsService} from './apps.service';
+import {AppService} from './app.service';
 
 @NgModule({
-  imports:      [ BrowserModule,HttpModule ],
-  declarations: [ MyCardComponent, LoginComponent, StoreComponent, LobbyComponent, CommunityComponent, AppsComponent, AppDetailComponent, RosterComponent, CandyComponent ],
-  bootstrap:    [ MyCardComponent ],
-  providers:    [ RoutingService, AppsService, AppService ],
+    imports: [BrowserModule, HttpModule],
+    declarations: [MyCardComponent, LoginComponent, StoreComponent, LobbyComponent, CommunityComponent, AppsComponent, AppDetailComponent, RosterComponent, CandyComponent],
+    bootstrap: [MyCardComponent],
+    providers: [RoutingService, AppsService, AppService],
+    schemas: [NO_ERRORS_SCHEMA]
 })
-export class MyCard { }
+export class MyCard {
+}
