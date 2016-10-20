f010d597792b4690a6ce34912878f43a04a82d42
zh99998 <zh99998@gmail.com>
2016-10-20 18:17:20 +0800

    decks

--- app/ygopro.component.ts
+++ app/ygopro.component.ts
@@ -5,13 +5,12 @@ import {Component} from '@angular/core';
 import {AppsService} from "./apps.service";
 import {RoutingService} from "./routing.service";
 
+declare var process;
 declare var System;
 const fs = System._nodeRequire('fs');
 const path = System._nodeRequire('path');
-const Promise = System._nodeRequire('bluebird');
-Promise.resolve("foo").then(function (msg) {
-    console.log(msg)
-});
+//const Promise = System._nodeRequire('bluebird');
+const ini = System._nodeRequire('ini');
 
 @Component({
     selector: 'ygopro',
@@ -21,6 +20,11 @@ Promise.resolve("foo").then(function (msg) {
 export class YGOProComponent {
     app = this.appsService.searchApp('ygopro');
     decks = [];
+    current_deck;
+
+    system_conf = path.join(this.app.local.path, 'system.conf');
+    numfont = this.get_font({'darwin': ['/System/Library/Fonts/PingFang.ttc']});
+    textfont = this.get_font({'darwin': ['/System/Library/Fonts/PingFang.ttc']});
 
     constructor(private appsService: AppsService, private routingService: RoutingService) {
         this.refresh()
@@ -29,6 +33,21 @@ export class YGOProComponent {
     refresh() {
         this.get_decks().then((decks)=> {
             this.decks = decks;
+            if (!(this.current_deck in this.decks)) {
+                this.current_deck = decks[0];
+            }
+        })
+    }
+
+    get_font(data) {
+        return new Promise((resolve, reject)=> {
+            let fonts = data[process.platform]
+        })
+    }
+
+    fix_fonts(ini) {
+        return new Promise((resolve, reject)=>{
+            this.numfont.then()
         })
     }
 
@@ -38,10 +57,16 @@ export class YGOProComponent {
                 if (error) {
                     reject(error)
                 } else {
-                    let result: string[] = files.filter(file=>path.extname(file) == ".ydk").map(file=>path.basename(file, '.ydk'));
-                    resolve(result);
+                    resolve(files.filter(file=>path.extname(file) == ".ydk").map(file=>path.basename(file, '.ydk')));
                 }
             })
         })
+    }
+
+    edit_deck(deck) {
+        fs.readFile(this.system_conf, {encoding: 'utf-8'}, (error, data) => {
+            if (error) throw error;
+            console.log(ini.parse(data));
+        });
     }
 }
