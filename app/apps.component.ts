518c78ba464e7cf83da6844d7f8d7251b8e327fa
zh99998 <zh99998@gmail.com>
2016-10-21 16:08:40 +0800

    app list

--- app/apps.component.ts
+++ app/apps.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { AppsService } from './apps.service'
-import { RoutingService } from './routing.service'
+import {Component} from '@angular/core';
+import {AppsService} from './apps.service'
+import {RoutingService} from './routing.service'
 
 @Component({
     selector: 'apps',
@@ -9,10 +9,10 @@ import { RoutingService } from './routing.service'
 })
 export class AppsComponent {
 
-    constructor(private appsService: AppsService, private routingService: RoutingService ) {
-        appsService.getApps(()=>{
+    constructor(private appsService: AppsService, private routingService: RoutingService) {
+        appsService.getApps(()=> {
             //console.log(appsService.data)
-            if(appsService.data.length > 0) {
+            if (appsService.data.length > 0) {
                 this.selectApp(appsService.data[0].id);
                 let tmp = this.appsService.data.filter((v)=>v.id === this.routingService.app);
                 //console.log(tmp);
@@ -27,20 +27,31 @@ export class AppsComponent {
         let data = this.appsService.data;
         let apps;
 
-        if(data) {
-            apps = this.appsService.data.filter((app)=>{
+        if (data) {
+            apps = this.appsService.data.filter((app)=> {
                 return contains.includes(app.category);
             });
         }
 
-        return apps;
+        return apps || [];
     }
 
     selectApp(id) {
         this.routingService.app = id;
         this.appsService.createInstallConfig(id);
+    }
 
+    get grouped_apps() {
+        let result = {'installed': [], 'not_installed': []};
 
+        for (let app of this.apps) {
+            if (app.local) {
+                result.installed.push(app)
+            } else {
+                result.not_installed.push(app)
+            }
+        }
+        return result
     }
 
 }
