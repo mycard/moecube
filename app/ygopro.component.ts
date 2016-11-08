3b65f412d97b28c544b9c590c55fee4045df058e
wudizhanche1000 <wudizhanche1000@gmail.com>
2016-11-08 18:18:04 +0800

    ygopro问题

--- app/ygopro.component.ts
+++ app/ygopro.component.ts
@@ -75,8 +75,7 @@ export class YGOProComponent implements OnInit {
     app: App;
     decks: string[] = [];
     current_deck: string;
-
-    system_conf = path.join(this.app.local.path, 'system.conf');
+    system_conf;
     numfont = {'darwin': ['/System/Library/Fonts/PingFang.ttc']};
     textfont = {'darwin': ['/System/Library/Fonts/PingFang.ttc']};
 
@@ -108,10 +107,12 @@ export class YGOProComponent implements OnInit {
     connections: WebSocket[] = [];
 
     constructor(private http: Http, private appsService: AppsService, private loginService: LoginService, private ref: ChangeDetectorRef) {
-        this.refresh();
     }
 
     ngOnInit() {
+        this.system_conf = path.join(this.app.local.path, 'system.conf');
+        this.refresh();
+
         let modal = $('#game-list-modal');
 
         modal.on('show.bs.modal', (event) => {
