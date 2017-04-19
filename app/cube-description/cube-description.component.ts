097000a94910c57f7f15f8a6f4398891b0043d7c
2breakegg <1216023881@qq.com>
2017-04-19 16:46:06 +0800

    shop interface

--- app/cube-description/cube-description.component.ts
+++ app/cube-description/cube-description.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by zh99998 on 16/9/2.
  */
-import {ChangeDetectorRef, Component, Input, Pipe} from '@angular/core';
+import {ChangeDetectorRef, Component, Input} from '@angular/core';
 import {Cube} from '../cube';
 
 
@@ -11,35 +11,19 @@ import {Cube} from '../cube';
   styleUrls: ['./cube-description.component.css'],
 })
 
-@Pipe({name: 'keys'})
-
 export class CubeDescriptionComponent {
 
   @Input()
   currentCube: Cube;
 
-  divOpacity: number[] = [1, 0, 0];
-  div1 = 1;
-  div2 = 0;
-  color1 = 'red';
-  hrefs = 'http://abc.efg.ijk';
   imgsrc: string[] = [
     'http://cdn.akamai.steamstatic.com/steam/apps/545980/ss_bfc8d95b53734e03998342b1def248f560c440e3.600x338.jpg?t=1492488208',
     'http://cdn.akamai.steamstatic.com/steam/apps/15370/0000004764.1920x1080.jpg?t=1447351397',
     'http://cdn.akamai.steamstatic.com/steam/apps/15370/0000004767.600x338.jpg?t=1447351397'
   ];
-
-  constructor(private ref: ChangeDetectorRef) {
-
-  }
-
-  transform(value, args: string[]): any {
-    let keys = [];
-    for (let key in value) {
-      keys.push({key: key, value: value[key]});
-    }
-    return keys;
-  }
+  divOpacity: number[] = this.imgsrc.map(function (val: any, key: any) {
+    return key === 0 ? 1 : 0;
+  });
 
   test(v: number): void {
     console.log(v);
@@ -56,22 +40,4 @@ export class CubeDescriptionComponent {
     console.log(this.divOpacity);
   }
 
-  showxx(num: number): void {
-    console.log(num);
-    this.div1 = 0;
-    this.div2 = 1;
-  }
-
-  changecolor() {
-    this.color1 = 'green';
-    console.log(this.color1);
-    this.ref.detectChanges()
-  }
-
-  ngStyle() {
-    let styles = {
-      'background': this.color1
-    }
-    return styles;
-  }
 }
